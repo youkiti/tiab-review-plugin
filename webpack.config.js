fd974c93699310c840034f765ba4e0f76fe5385e
youkiti <youkiti@gmail.com>
2025-12-26 12:21:42 +0900

    feat: add plugin icons, enable side panel API, and update OAuth client ID.

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
                 { from: 'src/popup/popup.css', to: 'popup/popup.css' },
                 { from: 'src/sidepanel/sidepanel.html', to: 'sidepanel/sidepanel.html' },
                 { from: 'src/sidepanel/sidepanel.css', to: 'sidepanel/sidepanel.css' },
+                { from: 'src/icons', to: 'icons' },
             ],
         }),
     ],
